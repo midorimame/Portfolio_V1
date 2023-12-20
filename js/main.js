c20e0371c0186230e1873d630a28037dc4696c4c
Seoyeon Kwon <sykwon97@naver.com>
2023-12-20 21:23:33 +0900

    Basic Page(main.html, main.css, Some function etc)

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// alert(window.innerHeight);
+
+const toggle = document.getElementById('toggle_button');
+const toggle_menu = document.getElementById('toggle_menu')
+const toggle_item = document.getElementById('toggle_item')
+function know() {
+
+
+  let is_checked = toggle.checked;
+  if (is_checked === true){
+    // console.log('참')
+    toggle_menu.style.display='block';
+    toggle_item.style.display='block';
+
+  }
+  else {
+    // console.log('거짓')
+    toggle_menu.style.display='none';
+    toggle_item.style.display='none';
+  }
+}
+
+
+// 웹 브라우저 사이즈가 조정되면 toggle 메뉴 사라짐
+// (checkbox.checked -> false, display none)
+window.addEventListener("resize", function(event) {
+  // console.log(document.body.clientWidth + 'and' + toggle.checked);
+  if (document.body.clientWidth >= 992){
+    toggle.checked = false;
+    toggle_menu.style.display='none';
+    toggle_item.style.display='none';
+  }
+})
+
+function prepare(){
+  alert('준비 중 입니다.');
+}
+
+function go_home(){
+  location.href="main.html";
+}
